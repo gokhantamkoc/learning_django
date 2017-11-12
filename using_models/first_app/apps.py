dbd5c687ecf3f8adaeb98c7bb6916c488a29601b
gokhanta <gokhan.tamkoc@comodo.com>
2017-11-12 19:40:40 +0300

    updated using models

--- using_models/first_app/apps.py
+++ using_models/first_app/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
-
+from first_app.models import Topic, Webpage, AccessRecord
 
 class FirstAppConfig(AppConfig):
     name = 'first_app'
+
+admin.site.register(Topic)
+admin.site.register(Webpage)
+admin.site.register(AccessRecord)
